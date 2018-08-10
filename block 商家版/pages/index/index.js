a7703122d935c2abe51b663b001b4803ab855b76
849203846@qq.com <849203846@qq.com>
2018-08-10 18:01:42 +0800

    e

--- block 商家版/pages/index/index.js
+++ block 商家版/pages/index/index.js
@@ -9,13 +9,25 @@ Page({
       '/images/banner2.jpg',
     ],
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    pay:'',
+    work_exp:'',
+    shaixuan:'none',
+    education:''
   },
   //事件处理函数
   gotosousuo: function() {
     wx.navigateTo({
       url: '../sousuo/sousuo'
     })
+  },
+  swiperchange:function(e){
+    console.log(e.detail.current)
+    var index = e.detail.current
+    this.setData({
+      job_id: this.data.list[index].job_id
+    })
+    this.getposition()
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -47,22 +59,51 @@ Page({
   },
   onShow:function(){
     app.getuser()
-    // getposition
-    var data={
+    utils.sendRrquest('getpositionnum', 1, {})
+      .then((res) => {
+        console.log(res.data)
+        this.setData({
+          list: res.data.data,
+          job_id:res.data.data[0].job_id
+        })
+        this.getposition()
+      })
+
+  },
+  getposition:function(){
+    var data = {
       job_id: this.data.job_id,//职位id
       education: this.data.education,//学历0=>'初中及以下',1=>'中专/中技',2=>'高中',3=>'大专',4=>'本科',5=>'硕士',6=>'博士'
       work_exp: this.data.work_exp,//工作经验    0应届生 1一年以内 2 1-3年 3 3-5年 4 5-10年 5 10年以上
       pay: this.data.pay,//薪资范围
 
     }
-    utils.sendRrquest('getposition', 1, data)
-    .then((res)=>{
-      console.log(res.data.data.data)
-      this.setData({
-        list:res.data.data.data,
-        page:'1'
+    utils.sendRrquest('getresumelist', 1, data)
+      .then((res) => {
+        console.log(res.data.data.data)
+        this.setData({
+          positionlist:res.data.data.data,
+          page: '1'
+        })
       })
+  },
+  seteducation:function(e){
+      this.setData({
+          education: e.target.dataset.education
+      })
+    this.getposition()
+  },
+  setpay:function(e){
+    this.setData({
+      pay: e.target.dataset.pay
     })
+    this.getposition()
+  },
+  setwork_exp:function(e){
+    this.setData({
+      work_exp: e.target.dataset.work_exp
+    })
+    this.getposition()
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -81,14 +122,45 @@ Page({
   onReachBottom: function () {
     var page = ++this.data.page
     var data={
-      page: page
+      page: page,
+      job_id: this.data.job_id,//职位id
+      education: this.data.education,//学历0=>'初中及以下',1=>'中专/中技',2=>'高中',3=>'大专',4=>'本科',5=>'硕士',6=>'博士'
+      work_exp: this.data.work_exp,//工作经验    0应届生 1一年以内 2 1-3年 3 3-5年 4 5-10年 5 10年以上
+      pay: this.data.pay,//薪资范围
+
     }
-    utils.sendRrquest('getposition', 1, data)
+    utils.sendRrquest('getresumelist', 1, data)
       .then((res) => {
         this.setData({
           page: page,
-          list: [...this.data.list, ...res.data.data.data]
+          positionlist: [...this.data.positionlist, ...res.data.data.data]
         })
       })
+  },
+  openshaixuan:function(){
+    if (this.data.shaixuan==='block'){
+      this.setData({
+        shaixuan:'none'
+      })
+      return
+    }
+    this.setData({
+      shaixuan:'block'
+    })
+  },
+  closeshaixuan:function(){
+    this.setData({
+      shaixuan: 'none'
+    })
+  },
+  cleardata:function(){
+    this.setData({
+      page: 1,
+      job_id: '',
+      education: '',//学历0=>'初中及以下',1=>'中专/中技',2=>'高中',3=>'大专',4=>'本科',5=>'硕士',6=>'博士'
+      work_exp: '',//工作经验    0应届生 1一年以内 2 1-3年 3 3-5年 4 5-10年 5 10年以上
+      pay:'',//薪资范围
+    })
+    this.getposition()
   }
 })
